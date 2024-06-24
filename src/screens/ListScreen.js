b4d5514b943c9eb1f5305f93cc43983273a847e3
LPastine <lucianopastine.tech@gmail.com>
2024-06-24 15:31:20 -0300

    flat list excercise

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,54 +1,35 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { FlatList } from 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    {
-      name: 'Friend #1'
-    },
-    {
-      name: 'Friend #2'
-    },
-    {
-      name: 'Friend #3'
-    },
-    {
-      name: 'Friend #4'
-    },
-    {
-      name: 'Friend #5'
-    },
-    {
-      name: 'Friend #6'
-    },
-    {
-      name: 'Friend #7'
-    },
-    {
-      name: 'Friend #8'
-    },
-    {
-      name: 'Friend #9'
-    },
+    { name: 'Friend #1', age: 'Age 20' },
+    { name: 'Friend #2', age: 'Age 45' },
+    { name: 'Friend #3', age: 'Age 32' },
+    { name: 'Friend #4', age: 'Age 27' },
+    { name: 'Friend #5', age: 'Age 53' },
+    { name: 'Friend #6', age: 'Age 30' },
+    { name: 'Friend #7', age: 'Age 33' },
   ];
+
   return (
-    <FlatList 
-      horizontal
-      showsHorizontalScrollIndicator={false}
+    <FlatList
       keyExtractor={(friend) => friend.name}
-      data={friends} 
+      data={friends}
       renderItem={({ item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>{item.name} - {item.age}</Text>
+        );
       }}
     />
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical: 50,
-  }
-})
+    fontSize: 20,
+    marginVertical: 20,
+  },
+});
 
-export default ListScreen
+export default ListScreen;
